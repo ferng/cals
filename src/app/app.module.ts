26dd4e1788ebfbdb974ce224c41fcfb66cf4800e
ferng001 <ferng001@gmail.com>
2019-07-30 07:28:23 +0100

    add input fields, typography

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import { AppComponent } from './app.component';
 import { FoodItemComponent } from './food-item/food-item.component';
 
@@ -13,7 +17,11 @@ import { FoodItemComponent } from './food-item/food-item.component';
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatGridListModule
   ],
   declarations: [
     AppComponent,
