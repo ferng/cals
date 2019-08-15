2f9b4a4fe35092a10891527e8bba88bcebe90a2f
ferng001 <ferng001@gmail.com>
2019-08-15 07:21:57 +0100

    enter cals or grams to calculate the other

--- src/app/food-item/food-item.component.ts
+++ src/app/food-item/food-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { Food, Item, FoodService } from './food-item.service';
 
 @Component({
@@ -13,17 +14,63 @@ export class FoodItemComponent implements OnInit {
   error: any;
   headers: string[];
   items: Item[];
-  selected: string;
+	per100: number;
+  selection: string;
 
-  constructor(private foodService: FoodService) { }
+  constructor(private foodService: FoodService, private fb: FormBuilder) { }
 
   ngOnInit () {
-    this.showFoodResponse();
+    this.loadFoodResponse();
+  }
+
+	foodForm = this.fb.group({
+		selectedCal : [''],
+		grams : [''],
+		cals : [''],
+	});	
+
+	newSelection(event) {
+		this.updateCals();
+	}
+
+	calcCalsFromGrams() {
+    this.updateCals();
+	}
+ 
+  calcGramsFromCals() {
+    this.updateGrams();
+  }
+
+  updateCals() {
+    let selCal = this.foodForm.value.selectedCal;
+    let grams = this.foodForm.value.grams;
+    let cals = this.foodForm.value.cals;
+ 
+    if (this.notBlank(grams) && this.notBlank(selCal)) {
+      this.foodForm.value.cals = Math.ceil((Number.parseInt(selCal) / 100) * Number.parseInt(grams));
+      this.foodForm.setValue(this.foodForm.value);
+    }
+    console.log(this.foodForm.value);
+
   }
   
-  
+  updateGrams() {
+    let selCal = this.foodForm.value.selectedCal;
+    let grams = this.foodForm.value.grams;
+    let cals = this.foodForm.value.cals;
+ 
+    if (this.notBlank(cals) && this.notBlank(selCal)) {
+      this.foodForm.value.grams = Math.floor((Number.parseInt(cals) / Number.parseInt(selCal)) * 100);
+      this.foodForm.setValue(this.foodForm.value);
+    }
+    console.log(this.foodForm.value);
+  }
 
-  showFoodResponse() {
+  notBlank(data: string) {
+    return (data !== null && data !== undefined && data !== "");
+  }
+
+  loadFoodResponse() {
     this.foodService.getFoodResponse()
       .subscribe(resp => {
         const keys = resp.headers.keys();
@@ -32,7 +79,6 @@ export class FoodItemComponent implements OnInit {
         var food = { ... resp.body };
         
         this.items = (Object.keys(food).map(e=>food[e]))[0];
-        console.log(this.items[0]);
       });
   }
 }
